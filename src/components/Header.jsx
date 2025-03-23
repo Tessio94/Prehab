aab7cb6ffb897e5de733139b4ed1bfdf803921cc
Tessio94 <simeklapan@gmail.com>
2025-03-24 00:03:34 +0100

    logo fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -67,6 +67,8 @@ const Header = ({ header }) => {
             src="/images/logo/ikona.png"
             alt="logo"
             className="w-20 h-20 rounded-xl shadow-2xl"
+            height="80"
+            width="80"
           />
           <h1 className="lg:text-3xl text-2xl font-bold font-oswald">Prehab</h1>
         </Link>
